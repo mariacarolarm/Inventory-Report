fea6946ebd0409889948e7b7847d0b1fc590670a
Maria <marycarol.00@gmail.com>
2024-07-31 17:16:37 -0300

    feat: setimo requisito completo e nono requisito incompleto

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,26 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    def generate(self) -> str:
+        basic_report = super().generate()
+
+        all_products = []
+        for inventory in self.inventories:
+            all_products.extend(inventory.data)
+
+        company_inventory_count = Counter(
+            product.company_name for product in all_products
+        )
+
+        company_stock_report = "\n".join(
+            f"- {company}: {count}" for company,
+            count in company_inventory_count.items()
+        )
+
+        return (
+            f"{basic_report}\n"
+            f"Stocked products by company:\n"
+            f"{company_stock_report}"
+        )
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,51 @@
+from typing import List
+from datetime import datetime
+from inventory_report.inventory import Inventory
+
+
 class SimpleReport:
-    pass
+    def __init__(self):
+        self.inventories: List[Inventory] = []
+
+    def add_inventory(self, inventory: Inventory) -> None:
+        self.inventories.append(inventory)
+
+    def get_oldest_date(self) -> str:
+        return min(
+            product.manufacturing_date for inventory in
+            self.inventories for product in inventory.data
+        )
+
+    def get_expiration_date(self) -> str:
+        return min(
+            product.expiration_date for inventory in
+            self.inventories for product in inventory.data
+            if datetime.strptime(product.expiration_date,
+                                 "%Y-%m-%d") >= datetime.now()
+        )
+
+    def get_largest_inventory(self) -> str:
+        all_products = []
+        for inventory in self.inventories:
+            all_products.extend(inventory.data)
+
+        company_inventory_count = {}
+        for product in all_products:
+            company_inventory_count[product.company_name] = (
+                company_inventory_count.get(product.company_name, 0) + 1
+            )
+        largest_inventory = max(company_inventory_count,
+                                key=company_inventory_count.get)
+
+        return largest_inventory
+
+    def generate(self) -> str:
+        oldest = self.get_oldest_date()
+        closest = self.get_expiration_date()
+        largest_inventory = self.get_largest_inventory()
+
+        return (
+            f"Oldest manufacturing date: {oldest}\n"
+            f"Closest expiration date: {closest}\n"
+            f"Company with the largest inventory: {largest_inventory}"
+        )
